8ee81721c647703ee24c5c4172e7c147b8966ca2
DamaMichaelYohanna <get2dama11@gmail.com>
2026-08-11 14:32:00 +0100

    style: remove unused import in profile_screen.dart

--- laundry_mobile/lib/features/settings/profile_screen.dart
+++ laundry_mobile/lib/features/settings/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lucide_icons/lucide_icons.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../core/theme.dart';
 import '../../core/providers.dart';
